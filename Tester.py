8ff01bc5d5d650b34bd8a19625a865941a6a5d72
ImalshaD <plid475@gmail.com>
2023-10-30 13:06:31 +0530

    Printing error fixed

--- Tester.py
+++ Tester.py
@@ -10,7 +10,6 @@ img = cv2.imread(r'Test_Images\Tables\20231019_092445.jpg')
 x=ocr.detectText(img)
 l=[]
 for i in x[1:]:
-    print(i.description)
     b=Box(i.bounding_poly.vertices[0].x,i.bounding_poly.vertices[1].x,i.bounding_poly.vertices[2].x,i.bounding_poly.vertices[3].x,i.bounding_poly.vertices[0].y,i.bounding_poly.vertices[1].y,i.bounding_poly.vertices[2].y,i.bounding_poly.vertices[3].y)
     c=Cell(i.description,b)
     l.append(c)
